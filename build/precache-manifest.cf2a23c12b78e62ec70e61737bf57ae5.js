73d11fa9dab48264527b0e515b3c18ede9d103f5
nashvincent <36297311+nashvincent@users.noreply.github.com>
2020-04-22 16:59:32 +0530

    Added static frontend files

---
+++ build/precache-manifest.cf2a23c12b78e62ec70e61737bf57ae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323538429c2aa442b2d8821af4405861",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50550b7c332b1f68933f",
+    "url": "/static/css/main.55f1cae1.chunk.css"
+  },
+  {
+    "revision": "c304a1bb1127a3725e08",
+    "url": "/static/js/2.411224bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.411224bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50550b7c332b1f68933f",
+    "url": "/static/js/main.4e1bea76.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
