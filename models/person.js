bde237acd1ec81e8f8828329773e5cecfd8c8b04
nashvincent <36297311+nashvincent@users.noreply.github.com>
2020-04-23 20:12:37 +0530

    fixes

--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URL
+const url = process.env.MONGODB_URI
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => console.log("Connected to MongoDB"))
     .catch(error => console.log('error connecting to MongoDB', error.message))
